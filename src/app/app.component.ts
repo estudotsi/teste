092659b45e2cdcb55d28e61c1499014f6eea477e
Rogerio Martins Campos Martins Campos <zzmelzz@hotmail.com>
2023-11-01 17:34:11 -0300

    teste otp and reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { NgOtpInputComponent, NgOtpInputConfig } from 'ng-otp-input';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,80 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'teste';
+
+  configNome :NgOtpInputConfig = {
+    allowNumbersOnly: false,
+    length: 6,
+    isPasswordInput: false,
+    disableAutoFocus: false,
+    placeholder: '',
+    inputStyles: {
+      'width': '40px',
+      'height': '40px'
+    }
+  };
+
+  @ViewChild('d2InputField') d2InputField?: ElementRef;
+  @ViewChild('d3InputField') d3InputField?: ElementRef;
+
+  form: FormGroup;
+  nome?: string;
+  total?: number;
+
+  /*constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      d1: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      d2: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      m1: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      m2: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      a1: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      a2: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      a3: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      a4: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]]
+    });
+  }*/
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      d1: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      d2: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]],
+      d3: ['', [Validators.required, Validators.minLength(1), Validators.maxLength(1)]]
+    });
+  }
+
+  /*onNameInput() {
+    if (this.form.get('d1')?.valid) {
+      // Set focus to the other input field
+      this.otherInputField.nativeElement.focus();
+    }
+  }*/
+
+  d1Input() {
+    if (this.form.get('d1')?.valid) {
+      // Set focus to the other input field
+      this.d2InputField?.nativeElement.focus();
+    }
+  }
+
+  d2Input() {
+    if (this.form.get('d2')?.valid) {
+      // Set focus to the other input field
+      this.d3InputField?.nativeElement.focus();
+    }
+  }
+
+  submitForm() {
+    if (this.form.valid) {
+      console.log('Form submitted:', this.form.value);
+      console.log('Otp: ', this.nome);
+    }
+  }
+
+  onOtpChangeNome(otpNome: string) {
+    this.nome = otpNome;
+    this.total = this.nome.length;
+
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import {NgxMaskModule} from 'ngx-mask';
+import { NgOtpInputModule } from  'ng-otp-input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    NgOtpInputModule,
+    NgxMaskModule.forRoot({
+      showMaskTyped : true,
+      // clearIfNotMatch : true
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
